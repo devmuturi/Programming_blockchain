d4a4d572fa3e57951648ef1d74b49cf7d39f43dc
devmuturi <muturinjonge09@gmail.com>
2025-11-05 00:24:21 +0300

    Create blockchain by linking the crypto blocks

--- lib/crypto_hashes.rb
+++ lib/crypto_hashes.rb
@@ -3,17 +3,18 @@ require "digest"
 require "pp"
 
 class Block
-  attr_reader :data, :hash, :nonce
+  attr_reader :data, :hash, :nonce, :prev
 
-  def initialize(data)
+  def initialize(data, prev)
     @data = data
+    @prev = prev
     @nonce, @hash = compute_hash_with_proof_of_work
   end
 
   def compute_hash_with_proof_of_work(difficulty = "0000")
     nonce = 0
     loop do
-      hash = Digest::SHA256.hexdigest("#{nonce}#{data}")
+      hash = Digest::SHA256.hexdigest("#{nonce}#{prev}#{data}")
       return [nonce, hash] if hash.start_with?(difficulty)
 
       nonce += 1
@@ -21,4 +22,12 @@ class Block
   end
 end
 
-pp Block.new("Njonge")
+# Build some blocks linked(chained) together with crypto hashes
+b0 = Block.new("Hello, Cryptos!", "0000000000000000000000000000000000000000000000000000000000000000")
+b1 = Block.new("Hello, Cryptos! - Hello, Cryptos!", b0.hash)
+b2 = Block.new("Your Name Here", b1.hash)
+b3 = Block.new("Data Data Data Data", b2.hash)
+
+blockchain = [b0, b1, b2, b3]
+
+pp blockchain
