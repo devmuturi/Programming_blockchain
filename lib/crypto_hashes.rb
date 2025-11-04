b911aae33c1057c9473327c14f78c711fd58d8e5
devmuturi <muturinjonge09@gmail.com>
2025-11-05 00:04:46 +0300

    Add a proof of work to block and hash

--- lib/crypto_hashes.rb
+++ lib/crypto_hashes.rb
@@ -3,12 +3,22 @@ require "digest"
 require "pp"
 
 class Block
-  attr_reader :data, :hash
+  attr_reader :data, :hash, :nonce
 
   def initialize(data)
     @data = data
-    @hash = Digest::SHA256.hexdigest(data)
+    @nonce, @hash = compute_hash_with_proof_of_work
+  end
+
+  def compute_hash_with_proof_of_work(difficulty = "0000")
+    nonce = 0
+    loop do
+      hash = Digest::SHA256.hexdigest("#{nonce}#{data}")
+      return [nonce, hash] if hash.start_with?(difficulty)
+
+      nonce += 1
+    end
   end
 end
 
-pp Block.new("Hello, Cryptos!")
+pp Block.new("Njonge")
